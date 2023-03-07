c16705453e6835b71647e26f3147d25d0dddb883
Luana DNS <ludiasnsilva@gmail.com>
2023-03-07 19:42:18 +0000

    AddMoreButton is working, solved some type errors, addedsome comments, now createNewSubject is not working

--- client/src/components/CreateQuestionForm.js
+++ client/src/components/CreateQuestionForm.js
@@ -1,107 +1,191 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom"; //useNavigate not added yet
 import Title from "./Title";
 const BASE_URL = "http://localhost:5000";
 
 // This component is responsible for rendering a form for creating a new question
+// Define the CreateQuestionForm functional component that takes an onSubmit function as a prop
 function CreateQuestionForm({ onSubmit }) {
-  const navigate = useNavigate();
-  const [question, setQuestion] = useState({
-    question: "",
-    answer: "",
-  });
+	// Call the "useNavigate" hook to get a reference to the navigation object
+	const navigate = useNavigate();
+	const [error, setError] = useState("");
+	let [response, setResponse] = useState("");
 
-  const { id } = useParams();
+	// Call the "useState" hook to define the "question" state variable and the "setQuestion" function that updates it
+	const [question, setQuestion] = useState([
+		{
+			question: "",
+			answer: ""
+		}
+	]);
 
-  const handleChange = (e) => {
-    const target = e.target;
-    const name = target.name;
-    const value = target.value;
+	const addInputField = () => {
+		setQuestion([
+			...question, //adding the information that I already have
+			{
+				//creating a new object to add more questions and answers
+				question: "",
+				answer: ""
+			}
+		]);
+	};
 
-    setQuestion((question) => {
-      return {
-        ...question,
-        [name]: value,
-      };
-    });
-  };
+	// Call the "useParams" hook to get the "id" parameter from the URL
+	const { id } = useParams();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setQuestion({
-      question: "",
-      answer: "",
-    });
-    addQuestion(question);
-  };
+	// Define the "handleInputChange" function that updates the "question" state variable whenever a form input is changed
+	/* const handleInputChange = e => {
+		const target = e.target;
+		const name = target.name;
+		const value = target.value;
 
-  const addQuestion = async (question) => {
-    try {
-      await fetch(`${BASE_URL}/questions`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/zjson",
-        },
-        body: JSON.stringify({ subject_id: id, ...question }),
-      });
-      navigate(`/subjects/${id}/questions`);
-    } catch (err) {
-      console.log("Oops, something went wrong");
-    }
-  };
-  return (
-    <>
-      <Title />
-      <div className="flex flex-col border-opacity-50 justify-center items-center">
-        <div className="grid card bg-green-200 m-5 p-5 w-4/5 text-justifyrounded-box place-items-center shadow-xl object-scale-down sm:object-contain sm:m-10 sm:p-10 sm:w-3/5">
-          <div className="btnform-control w-full max-w-xs">
-            <h2 className="text-lg tracking-widest mb-5">
-              Add questions in subject {id}:
-            </h2>
-          </div>
-          <div className="btncard-body">
-            <form onSubmit={handleSubmit}>
-              <label className="btnlabel font-semibold">
-                Question:
-                <input
-                  type="text"
-                  name="question"
-                  className=" btninput-bordered btninput border border-solid rounded border-gray-300  px-1  font-light w-full max-w-xs"
-                  value={question.question}
-                  onChange={(e) => handleChange(e)}
-                />
-              </label>
-              <br />
-              <label className="btnlabel font-semibold">
-                Answer:
-                <input
-                  type="text"
-                  name="answer"
-                  className=" btninput-bordered btninput border border-solid rounded border-gray-300  px-1  font-light w-full max-w-xs"
-                  value={question.answer}
-                  onChange={(e) => handleChange(e)}
-                />
-              </label>
-              <div>
-                <button className="btn btn-sm bg-accent-focus marg mt-10">
-                  Back{" "}
-                  {/*not sure why this works but it goes back to the questions*/}
-                </button>{" "}
-                {/* <button className="btn btn-sm bg-accent-focus marg mt-10">
+		setQuestion(question => {
+			return {
+				...question,
+				[name]: value
+			};
+		});
+	}; */
+	const onQuestionChange = index => event => {
+		const value = event.target.value;
+		setQuestion(
+			question.map((input, inputIndex) =>
+				index === inputIndex
+					? {
+							question: value,
+							answer: question[index].answer
+					  }
+					: input
+			)
+		);
+	};
+
+	const onAnswerChange = index => event => {
+		const value = event.target.value;
+		setQuestion(
+			question.map((input, inputIndex) =>
+				index === inputIndex
+					? {
+							question: question[index].question,
+							answer: value
+					  }
+					: input
+			)
+		);
+	};
+
+	// Define the "handleSubmit" function that submits the form data to the server
+	/* const handleSubmit = e => {
+		e.preventDefault();
+		setQuestion({
+			question: "",
+			answer: ""
+		});
+		addQuestion(question);
+	}; */
+	const handleSubmit = event => {
+		event.preventDefault(); //prevents from refreshing
+		const questionsAndAnswers = setQuestion.map(input => ({ question: input.question, answer: input.answer }));
+		addQuestion(question.question, question.answer, questionsAndAnswers) //sending the input info to the back end
+			//the back end is gonna return an answer with the listId (used on post back end)
+			.then(fetchResponse => {
+				//the api returns the id that it's created
+				console.log(fetchResponse.listId);
+				setResponse(fetchResponse.listId);
+			})
+			.catch(error => {
+				setError(`List was not created: ${error}`); //in case of error returns it
+			});
+		setQuestion([
+			{
+				question: "",
+				answer: ""
+			}
+		]);
+	};
+
+	// Define the "addQuestion" function that sends a POST request to the server to add a new question to the database
+	const addQuestion = async question => {
+		try {
+			await fetch(`${BASE_URL}/questions`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/zjson"
+				},
+				body: JSON.stringify({ subject_id: id, ...question })
+			});
+			//redirecting the user to a page that displays a list of questions for a specific subject based on the id parameter in the URL.
+			navigate(`/subjects/${id}/questions`);
+		} catch (err) {
+			console.log("Oops, something went wrong");
+		}
+	};
+	return (
+		<>
+			<Title />
+			<div className="flex flex-col border-opacity-50 justify-center items-center">
+				<div className="grid card bg-green-200 m-5 p-5 w-4/5 text-justifyrounded-box place-items-center shadow-xl object-scale-down sm:object-contain sm:m-10 sm:p-10 sm:w-3/5">
+					<div className="btnform-control w-full max-w-xs">
+						<h2 className="text-lg tracking-widest mb-5">Add questions in subject {id}:</h2>
+					</div>
+					<div className="btncard-body">
+						<form onSubmit={handleSubmit}>
+							<section>
+								{/* using map to loop through my inputs */}
+								{question.map((input, index) => {
+									return (
+										/* fragment does nothing, but it's necessary because i'm returning more than one thing just holds the key */
+										<Fragment key={index}>
+											<div>
+												<label className="btnlabel font-semibold">
+													Question:
+													<input
+														type="text"
+														name="question"
+														className=" btninput-bordered btninput border border-solid rounded border-gray-300  px-1  font-light w-full max-w-xs"
+														value={input.question}
+														onChange={onQuestionChange(index)}
+														placeholder={`Question ${index + 1}`}
+													/>
+												</label>
+											</div>
+											{/* <br /> */}
+											<div>
+												<label className="btnlabel font-semibold">
+													Answer:
+													<input
+														type="text"
+														name="answer"
+														className=" btninput-bordered btninput border border-solid rounded border-gray-300  px-1  font-light w-full max-w-xs"
+														value={input.answer}
+														onChange={onAnswerChange(index)}
+														placeholder={`Answer ${index + 1}`}
+													/>
+												</label>
+											</div>
+										</Fragment>
+									);
+								})}
+							</section>
+							<div>
+								<button onClick={addInputField} type="button" className="add-more">
+									Add more presents
+								</button>
+								<button className="btn btn-sm bg-accent-focus marg mt-10">
+									Back {/*not sure why this works but it goes back to the questions*/}
+								</button>{" "}
+								{/* <button className="btn btn-sm bg-accent-focus marg mt-10">
                   Add more questions
                 </button> */}
-                <button
-                  type="submit"
-                  className="btn btn-sm bg-accent-focus marg mt-10"
-                >
-                  Submit
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+								<button type="submit" className="btn btn-sm bg-accent-focus marg mt-10">
+									Submit
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 export default CreateQuestionForm;
--- routes/questions.js
+++ routes/questions.js
@@ -4,13 +4,13 @@ const db = require("../model/helper");
 
 //GET all posts
 router.get("/", async (req, res) => {
-  try {
-    const response = await db("SELECT * from questions");
-    const questions = response.data;
-    res.send({ questions });
-  } catch (err) {
-    res.status(500).send(err);
-  }
+	try {
+		const response = await db("SELECT * from questions");
+		const questions = response.data;
+		res.send({ questions });
+	} catch (err) {
+		res.status(500).send(err);
+	}
 });
 
 //not working due to issues with foreign key
@@ -25,16 +25,70 @@ router.get("/", async (req, res) => {
 
 //CREATE a question
 router.post("/", async (req, res) => {
-  const question = req.body.question;
-  const answer = req.body.answer;
-  const subject_id = req.body.subject_id;
-  try {
-    const addQuestion = await db(
-      `INSERT INTO questions (question, answer, subject_id) VALUES    ("${question}","${answer}","${subject_id}")`
-    );
-    res.status(200).send({ message: "question created" });
-  } catch (err) {
-    res.status(500).send(err);
-  }
+	const question = req.body.question;
+	const answer = req.body.answer;
+	const subject_id = req.body.subject_id;
+	try {
+		const addQuestion = await db(
+			`INSERT INTO questions (question, answer, subject_id) VALUES    ("${question}","${answer}","${subject_id}")`
+		);
+		res.status(200).send({ message: "question created" });
+	} catch (err) {
+		res.status(500).send(err);
+	}
 });
+
+router.post("/create", async (req, res) => {
+	const subject = req.body.subject;
+	try {
+		const existingSubject = await db(`SELECT * FROM subjects WHERE subject="${subject}"`);
+		if (existingSubject.data.length > 0) {
+			return res.status(400).send({ error: "Subject already exists" });
+		}
+		const addSubject = await db(`INSERT INTO subjects (subject) VALUE ("${subject}")`);
+		if (addSubject.insertId) {
+			const insertId = addSubject.insertId;
+			const questionsList = req.body.presentList;
+			for (let i = 0; i < questionsList.length; i++) {
+				const question = questionsList[i].question;
+				const answer = questionsList[i].answer;
+				await db(
+					`INSERT INTO questions (question, answer, subject_id) VALUES ("${question}","${answer}", ${insertId});`
+				);
+			}
+			res.send({ subject_id: insertId });
+		} else {
+			res.status(400).send("Could not insert subject");
+		}
+	} catch (err) {
+		res.status(400).send({ error: err.message });
+	}
+});
+/* router.post("/create", async (req, res) => {
+	const subject = req.body.subject;
+	try {
+		console.log("Solicitud de back-end recibida");
+		const addQuestions = await db(`INSERT INTO subjects (subject) VALUE ("${subject}")`); // Insert a new question and answer into the database with the provided subject
+		// Now we need to add the questions
+		if (addQuestions.insertId) {
+			// If a new row was successfully inserted
+			const insertId = addQuestions.insertId; // Store the ID of the newly inserted row
+			const questionsList = req.body.presentList; // Retrieve present list from the request body
+			for (let i = 0; i < questionsList.length; i++) {
+				// Iterate through each question in the questions list
+				const question = questionsList[i].question; // Retrieve present name
+				const answer = questionsList[i].answer; // Retrieve present URL
+				await db(
+					// Insert a new row into the "questions" table with the question and answer and the ID of the newly inserted row in the "subjects" table
+					`INSERT INTO questions (question, answer, subject_id) VALUES ("${question}","${answer}", ${insertId});`
+				);
+			}
+			res.send({ subject_id: insertId }); // Return the ID of the newly created list to the client
+		} else {
+			res.status(400).send("Could not insert list"); // If a new row was not inserted, send an error response to the client
+		}
+	} catch (err) {
+		res.status(400).send({ error: err.message }); // If an error occurred, send an error response to the client with the error message
+	}
+}); */
 module.exports = router;
