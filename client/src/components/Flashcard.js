af88b20235a5a31ae2afe4769ff1d938236d6a23
Luana DNS <ludiasnsilva@gmail.com>
2023-03-13 18:08:06 +0000

    prev and next button padding

--- client/src/components/Flashcard.js
+++ client/src/components/Flashcard.js
@@ -1,37 +1,41 @@
-import React /* , { useEffect, useState }  */ from "react";
+import React, { Link } from "react";
 
 function Flashcard({ showAnswers, question, answer }) {
-  return (
-    <>
-      <div className="flex flex-col border-opacity-50 justify-center items-center">
-        <div className="grid card bg-green-200 m-5 p-5 w-4/5 text-justify rounded-box place-items-center shadow-xl object-scale-down sm:object-contain sm:m-10 sm:p-10 sm:w-3/5">
-          <div className="w-[300px] h-[420px] bg-transparent cursor-pointer group  ">
-            <div
-              className={`relative preserve-3d w-full h-full duration-1000 ${
-                showAnswers ? "my-rotate-y-180" : ""
-              }  }`}
-            >
-              <div className="card shadow absolute backface-hidden border-2 w-full h-full ">
-                <h1 class="card-body  text-center flex flex-col items-center justify-center h-full text-3xl font-semibold">
-                  {question}
-                </h1>
-              </div>
+	return (
+		<>
+			<div className="flex flex-col border-opacity-50 justify-center items-center">
+				<div className="grid card bg-green-200 m-5 p-5 w-4/5 text-justify rounded-box place-items-center shadow-xl object-scale-down sm:object-contain sm:m-10 sm:p-10 sm:w-3/5">
+					<div className="w-[300px] h-[420px] bg-transparent cursor-pointer group  ">
+						<div
+							className={`relative preserve-3d w-full h-full duration-1000 ${
+								showAnswers ? "my-rotate-y-180" : ""
+							}  }`}
+						>
+							<div className="card shadow absolute backface-hidden border-2 w-full h-full ">
+								<h1 className="card-body  text-center flex flex-col items-center justify-center h-full text-3xl font-semibold">
+									{question}
+								</h1>
+							</div>
 
-              <div className="card shadow absolute my-rotate-y-180 backface-hidden  bg-gray-100 w-full h-full ">
-                <div
-                  className={`card-body text-center flex flex-col items-center justify-center h-full text-gray-800 ${
-                    !showAnswers ? "bg-gray-800 " : ""
-                  }`}
-                >
-                  <p>{answer}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+							<div className="card shadow absolute my-rotate-y-180 backface-hidden  bg-gray-100 w-full h-full ">
+								<div
+									className={`card-body text-center flex flex-col items-center justify-center h-full text-gray-800 ${
+										!showAnswers ? "bg-gray-800 " : ""
+									}`}
+								>
+									<p>{answer}</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
-
+/*
+<Link to="/subjects">
+						<button className="btn btn-sm bg-accent-focus marg mt-10">X</button>
+					</Link>
+                     */
 export default Flashcard;
--- client/src/components/GameOver.js
+++ client/src/components/GameOver.js
@@ -1,18 +1,18 @@
 import Title from "./Title";
 
 export default function GameOver(currentQuestionIndex, questions) {
-  return (
-    <>
-      <div className="flex flex-col border-opacity-50 justify-center items-center">
-        <div className="grid card bg-green-200 m-5 p-5 w-4/5 text-justifyrounded-box place-items-center shadow-xl object-scale-down sm:object-contain sm:m-10 sm:p-10 sm:w-3/5">
-          <h2>GAME OVER</h2>
-          <div>
-            <h3>{questions.length - 1}</h3> {console.log(questions.length)}
-            <h3>{currentQuestionIndex.Number}</h3>
-            {console.log(currentQuestionIndex)}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="flex flex-col border-opacity-50 justify-center items-center">
+				<div className="grid card bg-green-200 m-5 p-5 w-4/5 text-justifyrounded-box place-items-center shadow-xl object-scale-down sm:object-contain sm:m-10 sm:p-10 sm:w-3/5">
+					<h2>GAME OVER</h2>
+					<div>
+						<h3>{questions.length - 1}</h3> {console.log(questions.length)}
+						<h3>{currentQuestionIndex.Number}</h3>
+						{console.log(currentQuestionIndex)}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
